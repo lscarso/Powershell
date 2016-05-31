51fc90245b70b31973e2d48f7d14780fbb3274d6
lscarso <lscarso@users.noreply.github.com>
2016-05-31 12:03:10 +0200

    Update Import-UserAttributes.ps1

--- Import-UserAttributes/Import-UserAttributes.ps1
+++ Import-UserAttributes/Import-UserAttributes.ps1
@@ -5,12 +5,15 @@ Compare and Import users attributes from a SVC File.
 .DESCRIPTION 
 Compare and Import users attributes from a SVC File.
 
-.PARAMETER $doit 
-If set to false no changes are written to AD (check only)
+.PARAMETER $Whatif 
+If present no changes are written to AD (check only)
 
-.PARAMETER $VAR* 
+.PARAMETER $importFile
+Path and Filename of CSV with samaccountname and attributes
 
 .EXAMPLE 
+.\Import_UserAttributes.ps1 -Attributes manager -importFile C:\Data\Working\usr.csv
+Import manager attribute from CSV file specified
 
 .NOTES 
 CSV must use ";" as delimiter
@@ -19,112 +22,106 @@ The username must be given as simple samaccountname
 Rowtitle for the username is "samaccountname"
 #>
 
-Clear-Variable VAR*
-#####################################################
-# File to import
-$importFile = "usr.csv"
-#####################################################
-# If set to false no changes are written to AD (check only)
-$doit = $FALSE
-#####################################################
-# What attributes to change
-# Set to false or comment out to disable the import / check of this attribute
-$VARdescription = $TRUE
-$VARinfo = $TRUE
-$VARdepartment = $TRUE
-# $VARemployeenumber = $TRUE
-$VARmobilephone = $TRUE
-# $VARcompany = $TRUE
-# $VARl = $TRUE
-$VARtelephonenumber = $TRUE
-# $VARdivision = $TRUE
-# $VARextensionattribute10 = $TRUE
-# $VARc = $TRUE
-$VARco = $FALSE
-$VARstreetaddress = $TRUE
-$VARpostalcode = $TRUE
-$VARst = $TRUE
-# $VARfacsimileTelephoneNumber = $TRUE
-$VARtitle = $TRUE
-# $VARextensionAttribute12 = $TRUE
-$VARphysicalDeliveryOfficeName = $TRUE
-#####################################################
+Param( 
+     [string]$importFile = "usr.csv", 
+     [switch]$whatif,
+     [string[]]$Attributes
+     ) 
 
-Function SET-ADattribute ([string]$VARinput){ 
-	
-		if ($adusr.$VARinput) { if ($user.$VARinput -eq $adusr.$VARinput) {  } else {
-			if ($doit) {
-				if ($user.$VARinput) {
-					set-aduser $adusr -replace @{$VARinput=$user.$VARinput} #-whatif
-				} else {
-					set-aduser $adusr -clear $VARinput #-whatif
-				}
-			}
-			#write-host -fore red "$VARinput :       $($adusr.$VARinput) -> $($user.$VARinput)"
-            write-host -fore red "$(($VARinput + ':').padright(30)) $($adusr.$VARinput) -> $($user.$VARinput)" 
-            #write-host -fore Red ("{0}: .padright($(30 - $VARinput.Length),' ') {1} -> {2}" -f $VARinput,$($adusr.$VARinput),$($user.$VARinput))
-            }
-		} else { if ($user.$VARinput) {
-			if ($doit) {
-				set-aduser $adusr -replace @{$VARinput=$user.$VARinput} #-whatif
-			}
-			write-host -fore yellow "$VARinput :       * $($user.$VARinput)"
-		} else {  } }
-	
-}
-
-
-#Inizialize array
 $arrayattributes = @()
 
-$nl = ""
-$nl ; $nl ; $nl ; $nl
-write-host -fore gray -back gray "------------------------------------"
-$nl
-if ($VARdescription) { write-host -fore green "Enabled:  description"; $arrayattributes += @("description")} else { write-host -fore gray "Disabled: description"}
-if ($VARinfo) { write-host -fore green "Enabled:  info"; $arrayattributes += @("info")} else { write-host -fore gray "Disabled: info"}
-if ($VARdepartment) { write-host -fore green "Enabled:  department"; $arrayattributes += @("department")} else { write-host -fore gray "Disabled: department"}
-if ($VARemployeenumber) { write-host -fore green "Enabled:  employeenumber"; $arrayattributes += @("employeenumber")} else { write-host -fore gray "Disabled: employeenumber"}
-if ($VARmobilephone) { write-host -fore green "Enabled:  mobilephone"; $arrayattributes += @("mobilephone")} else { write-host -fore gray "Disabled: mobilephone"}
-if ($VARcompany) { write-host -fore green "Enabled:  company"; $arrayattributes += @("company") } else { write-host -fore gray "Disabled: company"}
-if ($VARl) { write-host -fore green "Enabled:  l"; $arrayattributes += @("l") } else { write-host -fore gray "Disabled: l"}
-if ($VARtelephonenumber) { write-host -fore green "Enabled:  telephonenumber"; $arrayattributes += @("telephonenumber")} else { write-host -fore gray "Disabled: telephonenumber"}
-if ($VARdivision) { write-host -fore green "Enabled:  division"; $arrayattributes += @("division")} else { write-host -fore gray "Disabled: division"}
-if ($VARextensionattribute10) { write-host -fore green "Enabled:  extensionattribute10"; $arrayattributes += @("extensionattribute10")} else { write-host -fore gray "Disabled: extensionattribute10"}
-if ($VARc) { write-host -fore green "Enabled:  c"; $arrayattributes += @("c")} else { write-host -fore gray "Disabled: c"}
-if ($VARco) { write-host -fore green "Enabled:  co"; $arrayattributes += @("co")} else { write-host -fore gray "Disabled: co"}
-if ($VARstreetaddress) { write-host -fore green "Enabled:  streetaddress"; $arrayattributes += @("streetaddress")} else { write-host -fore gray "Disabled: streetaddress"}
-if ($VARpostalcode) { write-host -fore green "Enabled:  postalcode"; $arrayattributes += @("postalcode")} else { write-host -fore gray "Disabled: postalcode"}
-if ($VARst) { write-host -fore green "Enabled:  st"; $arrayattributes += @("st")} else { write-host -fore gray "Disabled: st"}
-if ($VARfacsimileTelephoneNumber) { write-host -fore green "Enabled:  facsimileTelephoneNumber"; $arrayattributes += @("facsimileTelephoneNumber")} else { write-host -fore gray "Disabled: facsimileTelephoneNumber"}
-if ($VARtitle) { write-host -fore green "Enabled:  title" } else { write-host -fore gray "Disabled: title"}
-if ($VARextensionAttribute12) { write-host -fore green "Enabled:  extensionAttribute12"; $arrayattributes += @("extensionAttribute12")} else { write-host -fore gray "Disabled: extensionAttribute12"}
-if ($VARphysicalDeliveryOfficeName) { write-host -fore green "Enabled:  physicalDeliveryOfficeName"; $arrayattributes += @("physicalDeliveryOfficeName")} else { write-host -fore gray "Disabled: physicalDeliveryOfficeName"}
-$nl
-write-host -fore gray -back gray "------------------------------------"
-$nl ; $nl
-if ($doit) { write-host -fore red -back yellow "CHANGES WILL BE PROCESSED!" } else { write-host -fore cyan "No changes will be made!" }
-$nl ; $nl
-read-host "Press enter to continue"
-$nl
+Clear-Variable VAR*, Csv*, ad*
+#Get-Variable -Exclude PWD,*Preference, importFile, whatif, Attributes | Remove-Variable -EA 0
 
-$users = import-csv $importFile -delimiter ";" -Encoding Default | select -first 7
+# If set to false no changes are written to AD (check only)
+$doit = !($whatif)
 
-foreach ($user in $users) {
-	$adusr = $NULL
-	$adusr = get-aduser $user.samaccountname -property *
-	write-host -fore gray -back gray "------------------------------------"
-	$nl
-	write-host -fore cyan "$($adusr.samaccountname):"
-    
-    foreach ($attribute in $arrayattributes){
-        if ($attribute) {
-            Set-ADattribute ($attribute)
-	    }
-    }
 
-	$nl
+Function SET-ADattribute ([string]$VARinput){ 
+        #Check for special Attributes
+        if ($VARinput -eq "Manager"){
+            $user.$VARinput = $(get-aduser $user.$VARinput).DistinguishedName
+        }
+        #Generic Attributes
+		if ($adusr.$VARinput) {
+            if ($user.$VARinput -eq $adusr.$VARinput) { 
+            } else {
+			    if ($doit) {
+				    if ($user.$VARinput) {
+					    set-aduser $adusr -replace @{$VARinput=$user.$VARinput} #-whatif
+				    } else {
+					    set-aduser $adusr -clear $VARinput #-whatif
+				    }
+			    }
+			    #write-host -fore red "$VARinput :       $($adusr.$VARinput) -> $($user.$VARinput)"
+                write-host -fore red "$(($VARinput + ':').padright(30)) $($adusr.$VARinput) -> $($user.$VARinput)" 
+                #write-host -fore Red ("{0}: .padright($(30 - $VARinput.Length),' ') {1} -> {2}" -f $VARinput,$($adusr.$VARinput),$($user.$VARinput))
+            }
+		} else {
+            if ($user.$VARinput) {
+			    if ($doit) {
+				    set-aduser $adusr -replace @{$VARinput=$user.$VARinput} #-whatif
+			    }
+			    write-host -fore yellow "$VARinput :       * $($user.$VARinput)"
+		    } else { }
+        }
 }
 
-write-host -fore gray -back gray "------------------------------------"
-$nl
+
+Try{
+       
+    $nl = ""
+    $nl ; $nl ; $nl ; $nl
+    write-host -fore gray -back gray "------------------------------------"
+    $nl
+
+    # Check Attributes passed on parameter with CSV header
+    $CsvAttributes = $(import-csv $importFile -delimiter ";" -Encoding Default | select -first 1 | Get-Member -MemberType Properties).Name
+    
+    If ($CsvAttributes -ccontains "samaccountname") {
+        Write-Host -fore green "samaccountname column is present"
+        $nl
+    } Else {
+        Write-Host -fore red "samaccountname column is not present!"
+        Exit
+    }
+
+    Foreach ($CsvAtt in $CsvAttributes) {
+        if ($CsvAtt -eq "samaccountname"){
+        write-host -fore green "Enabled:  $CsvAtt"; $arrayattributes += @($CsvAtt)
+        } Else {
+            if ($attributes -contains $CsvAtt) { write-host -fore green "Enabled:  $CsvAtt"; $arrayattributes += @($CsvAtt)} else { write-host -fore gray "Disabled: $CsvAtt"}
+        }
+    }
+
+    $nl
+    write-host -fore gray -back gray "------------------------------------"
+    $nl ; $nl
+
+    if ($doit) { write-host -fore red -back yellow "CHANGES WILL BE PROCESSED!" } else { write-host -fore cyan "No changes will be made!" }
+    $nl ; $nl
+    read-host "Press enter to continue"
+    $nl
+
+    $users = import-csv $importFile -delimiter ";" -Encoding Default #| select -first 1
+
+    foreach ($user in $users) {
+	    $adusr = $NULL
+	    $adusr = get-aduser $user.samaccountname -property *
+	    write-host -fore gray -back gray "------------------------------------"
+	    $nl
+	    write-host -fore cyan "$($adusr.samaccountname):"
+    
+        foreach ($attribute in $arrayattributes){
+                Set-ADattribute ($attribute)
+        }
+	    $nl
+    }
+
+    write-host -fore gray -back gray "------------------------------------"
+    $nl
+}
+catch [Exception]{
+    $ErrorMsg = "ERROR: $($_.Exception.InnerException)"
+    write-host -fore red "$(($_.Exception.ParamName + ':').padright(30)) $ErrorMsg"
+}
